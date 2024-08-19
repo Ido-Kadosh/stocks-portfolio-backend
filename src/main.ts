ffbd3ef896283d06c57dff4dc0fb7d0c3923ace4
Ido Kadosh <1234kadosh@gmail.com>
2024-08-19 13:10:17 +0300

    feat: enable validation for all app, add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  app.setGlobalPrefix('api');
+  await app.listen(3030);
 }
 bootstrap();
