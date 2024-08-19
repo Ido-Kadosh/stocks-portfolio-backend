445d22541c1ef5107db25f5298d2bb490ae75ab0
Ido Kadosh <1234kadosh@gmail.com>
2024-08-19 13:09:47 +0300

    feat: add all modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { StocksModule } from './stocks/stocks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    StocksModule,
+    HttpModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
